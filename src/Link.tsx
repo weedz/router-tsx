2fd5fc316bf4599fd89c8e3e6bd6eac6398f9a29
Linus Björklund <weedzcokie@gmail.com>
2020-09-21 19:16:07 +0200

    can't be bothered with specific props..

--- src/Link.tsx
+++ src/Link.tsx
@@ -3,34 +3,18 @@ import { h, Component, createRef } from "preact";
 
 type Props = {
     activeClassName: string
-    scrollIntoView?: boolean
+    scrollToWhenActive?: boolean
 } & preact.JSX.HTMLAttributes<HTMLAnchorElement>;
-
-const privateProps = ["className", "activeClassName", "scrollIntoView"];
 
 export class Link extends Component<Props> {
     unsubscribe?: Function;
-    activeClass: string;
     ref = createRef();
-    anchorProps!: preact.JSX.HTMLAttributes<HTMLAnchorElement>;
-    constructor(props: Props) {
-        super(props);
-        this.activeClass = props.activeClassName;
-        this.updateAnchorProps(this.props);
-    }
+
     componentWillUnmount() {
         this.unsubscribe && this.unsubscribe();
     }
     componentDidMount() {
         this.unsubscribe = subscribe(this.update);
-    }
-    componentWillReceiveProps(newProps: Props) {
-        this.updateAnchorProps(newProps);
-    }
-    updateAnchorProps(props: Props) {
-        this.anchorProps = Object.fromEntries(
-            Object.entries(props).filter(([key, _]) => !privateProps.includes(key))
-        );
     }
     update = (_: string) => {
         this.setState({});
@@ -40,11 +24,11 @@ export class Link extends Component<Props> {
 
         if (getCurrentUrl() === this.props.href) {
             classes += classes ? ` ${this.props.activeClassName}` : this.props.activeClassName;
-            if (this.props.scrollIntoView) {
+            if (this.props.scrollToWhenActive) {
                 this.ref.current && this.ref.current.base.scrollIntoView({inline: "end", block: "nearest"});
             }
         }
 
-        return <StaticLink ref={this.ref} {...this.anchorProps} className={classes} />
+        return <StaticLink ref={this.ref} {...this.props} className={classes} />
     }
 }
